bebb32a78ffa5abe05de675d2347213812f2fddb
Andika <andikaraditya1@gmail.com>
2024-01-11 16:37:51 +0700

    initialize vite project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-center text-4xl font-bold text-[#5D5FEF]'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
