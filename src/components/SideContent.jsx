7ae14fe8640380a8ba09e758204f3413e0517114
Andika <andikaraditya1@gmail.com>
2024-01-12 14:42:10 +0700

    add assets for side content

--- src/components/SideContent.jsx
+++ src/components/SideContent.jsx
@@ -1,15 +1,26 @@
 import React from "react"
+import ChartLine from "../assets/chart-lines.svg"
+import ellips1 from "../assets/ellips1.svg";
+import ellips2 from "../assets/ellips2.svg";
+import ellips3 from "../assets/ellips3.svg";
+import curve1 from "../assets/curve1.svg";
+import curve2 from "../assets/curve2.svg";
 
 function SideContent() {
     return (
         <div className="w-[270px] flex flex-col gap-4 2xl:w-[200px]">
-            <div className="bg-primary rounded-xl p-5 h-[300px] flex flex-col items-start justify-between">
-                <p className="text-white text-2xl w-4/5">See analytics of the Customer Clearly</p>
-                <button className="p-3 bg-white bg-opacity-20 rounded-xl text-white">
+            <div className="bg-primary rounded-xl p-5 h-[300px] flex flex-col items-start justify-between relative overflow-hidden">
+                <p className="text-white text-2xl w-4/5 z-40">See analytics of the Customer Clearly</p>
+                <button className="p-3 bg-white bg-opacity-20 rounded-xl text-white z-50">
                     See Analytics
                 </button>
+                <img src={curve1} className="absolute top-0 left-0 z-0" alt="" />
+                <img src={curve2} className="absolute top-0 left-0 z-0" alt="" />
+                <img src={ellips1} className="absolute bottom-0 right-0 z-20" alt="" />
+                <img src={ellips2} className="absolute bottom-0 right-0 z-10" alt="" />
+                <img src={ellips3} className="absolute bottom-0 right-0 z-0" alt="" />
             </div>
-            <div className="bg-gradient-to-b from-slate-100 to-slate-50 bg-opacity-10 p-5 flex-1 rounded-xl">
+            <div className="bg-gradient-to-b from-slate-100 to-slate-50 bg-opacity-10 p-5 flex-1 rounded-xl relative overflow-hidden">
                 <p className="text-3xl w-4/5 font-bold">
                     Top Menu <span className="text-[#F17300]">This Week</span>
                 </p>
@@ -24,6 +35,7 @@ function SideContent() {
                     <p className="top-menu-item">4. Nasi Ayam Serundeng</p>
                     <p className="top-menu-item">5. Nasi Goreng Seafood</p>
                 </div>
+                    <img src={ChartLine} className="absolute bottom-0 w-[100%] left-[-10px] scale-125" alt="" />
             </div>
         </div>
     )
